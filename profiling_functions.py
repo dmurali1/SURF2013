473e8d15b60fb98ec1a8ee239c56cce8ce8b5660
Danya Murali <dmurali1@umbc.edu>
2013-06-07 15:21:12 -0400

    Changed index so it is based on string that sorts the times. One run of polyxtal based on time run per function.

--- profiling_functions.py
+++ profiling_functions.py
@@ -1,5 +1,4 @@
 #file: profiling_functions.py
-
 from fipy import Grid1D, CellVariable, TransientTerm, DiffusionTerm, Viewer
 import cProfile
 import pstats 
@@ -9,23 +8,24 @@ import matplotlib.pyplot as plt
 import math
 import inspect
 
-
 class FiPyProfile:
     def __init__(self, runfunc, ncells, regenerate=False):
         self.runfunc = runfunc
         self.ncells = ncells
         self.regenerate = regenerate
+        for ncell in ncells:
+            if not os.path.exists(self.datafilestring(ncell)) or self.regenerate:
+                self.profile(ncell)
 
     def datafilestring(self, ncell):
         return "data/{runfunc}{ncell}.stats".format(runfunc=self.runfunc.func_name, ncell=ncell)
 
     def profile(self, ncell):
+        print ncell
         runFuncString = 'self.runfunc(ncell={ncell})'.format(ncell=ncell)
         cProfile.runctx(runFuncString, globals(), locals(), filename=self.datafilestring(ncell))
         
     def get_stats(self, ncell):
-        if not os.path.exists(self.datafilestring(ncell)) or self.regenerate:
-            self.profile(ncell)
         return pstats.Stats(self.datafilestring(ncell))
 
     def get_sorted_keys(self, ncell, sort_field="cumulative"):
@@ -33,29 +33,34 @@ class FiPyProfile:
         return sorted_stats.fcn_list
 
 
-    def get_time_for_function(self, function_key, ncell):
-        return self.get_stats(ncell).stats[function_key][3]
+    def get_time_for_function(self, function_key, ncell, index=3):
+        return self.get_stats(ncell).stats[function_key][index]
 
     @staticmethod
     def get_key_from_function_pointer(function_pointer):
         return (inspect.getfile(function_pointer), inspect.getsourcelines(function_pointer)[1], function_pointer.func_name)
 
-    def plot(self, keys):
+    def plot(self, keys, field="cumulative"):
+        stats = self.get_stats(self.ncells[0])
+        sort_args = stats.get_sort_arg_defs()[field]
+        index = sort_args[0][0][0]
 
         for key in keys:
             functionTimes = []
-            for ncell in ncells:
-                print ncell,
-                functionTimes.append(get_time_for_function(key, ncell))
-            plt.loglog(self.ncells, functionTimes, label = fuction_pointers.func_name)           
+            for ncell in self.ncells:
+              #  print ncell,
+                functionTimes.append(self.get_time_for_function(key, ncell, index))
+            plt.loglog(self.ncells, functionTimes, label = key[2])
+           # print key
 
         allTimes = []
-        runfunc_key = get_key_from_function_pointer(self.runfunc)
+        runfunc_key = self.get_key_from_function_pointer(self.runfunc)
         for ncell in self.ncells:
-            print ncell,
-            allTimes.append(self.get_time_for_function(runfunc_key, ncell)  
+          #  print ncell,
+            allTimes.append(self.get_time_for_function(runfunc_key, ncell, index))  
         plt.loglog(self.ncells, allTimes, label = "full profile")        
-
+        plt.ylabel(sort_args[1])
+        plt.xlabel("ncells")
         plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
                    ncol=3, fancybox=True, shadow=True)
 
