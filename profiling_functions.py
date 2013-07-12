f2bea02577a6c24adb3dc3414ded43a0b9737dd9
Mira Holford <mira.holford@nist.gov>
2013-07-12 12:19:21 -0400

    Separate viewing and profiling into different classes.

--- profiling_functions.py
+++ profiling_functions.py
@@ -12,17 +12,16 @@ from fipyprofile import FiPyProfile
 import datetime
 
 class FiPyProfileTime(FiPyProfile):
-    def __init__(self, runfunc, ncells, regenerate=False, funcString=None):
+    def __init__(self, runfunc, ncell, regenerate=False, funcString=None):
         self.runfunc = runfunc
-        self.ncells = ncells
+        self.ncell = ncell
         self.regenerate = regenerate
         self.funcString = funcString
-        for ncell in ncells:
-            if not os.path.exists(self.datafilestring(ncell)) or self.regenerate:
-                self.profile(ncell)
+        if not os.path.exists(self.datafilestring()) or self.regenerate:
+            self.profile()
 
-    def datafilestring(self, ncell):
-        return "{funcString}{ncell}.stats".format(funcString=self.funcString, ncell=ncell)
+    def datafilestring(self):
+        return "{funcString}{ncell}.stats".format(funcString=self.funcString, ncell=self.ncell)
 	# now = datetime.datetime.now() 
         # year = now.year
         # month = now.month
@@ -33,22 +32,21 @@ class FiPyProfileTime(FiPyProfile):
         # return dateString
 	#return str({now})[:16].replace(" ", "_")).stats.format(now=now)
 	
-    def profile(self, ncell):
-        print ncell,
-        runFuncString = 'self.runfunc(ncell={ncell})'.format(ncell=ncell)
-        cProfile.runctx(runFuncString, globals(), locals(), filename=self.datafilestring(ncell))
+    def profile(self):
+        runFuncString = 'self.runfunc(ncell={ncell})'.format(ncell=self.ncell)
+        cProfile.runctx(runFuncString, globals(), locals(), filename=self.datafilestring())
         
-    def get_stats(self, ncell):
-        return pstats.Stats(self.datafilestring(ncell))
+    def get_stats(self):
+        return pstats.Stats(self.datafilestring())
 
-    def get_sorted_keys(self, ncell, sort_field="cumulative"):
-        sorted_stats = self.get_stats(ncell).sort_stats(sort_field)
+    def get_sorted_keys(self, sort_field="cumulative"):
+        sorted_stats = self.get_stats().sort_stats(sort_field)
         return sorted_stats.fcn_list
 
     
-    def get_time_for_function(self, function_key, ncell, index=3):
+    def get_time_for_function(self, function_key, index=3):
         """index = 3 refers to cumulative time"""
-        stats =  self.get_stats(ncell).stats
+        stats =  self.get_stats().stats
         if function_key in stats:
             return stats[function_key][index]
         else:
@@ -58,8 +56,9 @@ class FiPyProfileTime(FiPyProfile):
     def get_key_from_function_pointer(function_pointer):
         return (inspect.getfile(function_pointer), inspect.getsourcelines(function_pointer)[1], function_pointer.func_name)
 
-    def plot(self, keys, field="cumulative", doFullProfile = True, shortLabel = True):
-        stats = self.get_stats(self.ncells[0])
+class ProfileViewer(object):
+    def plot(self, profilers, keys, field="cumulative", doFullProfile = True, shortLabel = True):
+        stats = profilers[0].get_stats()
         sort_args = stats.get_sort_arg_defs()[field]
         index = sort_args[0][0][0]
         
@@ -70,9 +69,13 @@ class FiPyProfileTime(FiPyProfile):
       
         for key in keys:
             functionTimes = []
-            for ncell in self.ncells:
-                print ncell,
-                functionTimes.append(self.get_time_for_function(key, ncell, index))
+            ncells = []
+            for profiler in profilers:
+                print profiler.ncell,
+                ncells.append(profiler.ncell)
+                functionTimes.append(profiler.get_time_for_function(key, index))
+            ncells = np.array(ncells)
+
             if key[0] == '~':
                 label = key[2]
             else:
@@ -83,23 +86,23 @@ class FiPyProfileTime(FiPyProfile):
                 label = fileName + ": " + key[2]
 
             label = r""+str(label).replace("_", "\_").replace("<", "$<$").replace(">", "$>$")
-            ax1.loglog(self.ncells, functionTimes, label = label)
+            ax1.loglog(ncells, functionTimes, label = label)
             print key[0], key[2]
             
         if doFullProfile:
             allTimes = []
-            runfunc_key = self.get_key_from_function_pointer(self.runfunc)
-            for ncell in self.ncells:
-                print ncell,
-                allTimes.append(self.get_time_for_function(runfunc_key, ncell))
-            ax1.loglog(self.ncells, allTimes, label = "full profile")        
+            runfunc_key = profilers[0].get_key_from_function_pointer(profilers[0].runfunc)
+            for profiler in profilers:
+                print profiler.ncell
+                allTimes.append(profiler.get_time_for_function(runfunc_key,))
+            ax1.loglog(ncells, allTimes, label = "full profile")        
 
         plt.ylabel(sort_args[1])
         plt.xlabel("ncells")
       
         gs.tight_layout(fig, rect=[0,0,1,1])
-        plt.loglog(self.ncells, self.ncells**2, label="$ncells^2$")
-        plt.loglog(self.ncells, self.ncells*np.log(self.ncells), label="$n\log(n)$")
+        plt.loglog(ncells, ncells**2, label="$ncells^2$")
+        plt.loglog(ncells, ncells*np.log(ncells), label="$n\log(n)$")
         plt.legend(bbox_to_anchor=(1.05, 1), loc=2, ncol=1, mode="wrap", borderaxespad=0., prop={'size': 12})
         plt.show() 
       #  plt.savefig("Polyxtal_5_slowest.png")
