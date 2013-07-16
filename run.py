6eef8df34fd42314a4dcb9f22fac73867bb4bd59
Danya Murali <dmurali1@umbc.edu>
2013-07-16 15:45:42 -0400

    MemoryViewer now has caching ability.

--- run.py
+++ run.py
@@ -1,12 +1,31 @@
-from profiling_functions import FiPyProfileTime
+from profiling_memory import MemoryViewer
 from polyxtal import PolyxtalSimulation
-from fipyprofile import FiPyProfile
+from polyxtal import func
 import numpy as np
-from coupled1D import CoupledSimulation
 
-ncells = np.array(np.logspace(1, 2, 2), dtype=int)
-polyxtal = PolyxtalSimulation()
-coupled = CoupledSimulation()
-prof = FiPyProfileTime(coupled.run, ncells, regenerate=True)
-keys = prof.get_sorted_keys(ncells[0], sort_field="cumulative")
-prof.plot(keys[:5], "cumulative", doFullProfile=True)
+ncells = np.array(np.logspace(1, 3, 25), dtype=int)
+memoryViewer = MemoryViewer(ncells, PolyxtalSimulation.setup, func, (14, 17, 103, 106))
+data = memoryViewer.generateData()
+
+files = ["data/data0_{ncell}_polyxtal".format(ncell=n) for n in ncells]
+memoryViewer.savedata(data, files)
+data = memoryViewer.readdata(files)
+memoryViewer.plot(data)
+
+
+
+
+# from fipy.terms.term import Term
+
+# ncells = np.array(np.logspace(1, 5, 5), dtype=int)
+# memoryViewer2 = MemoryViewer(ncells, Term.solve, func, (209,))
+# data = memoryViewer2.generateData()
+# files = ["data/data0_{ncell}_solver".format(ncell=n) for n in ncells]
+# memoryViewer2.savedata(data, files)
+# data = memoryViewer.readdata(files)
+# memoryViewer2.plot(data)
+
+
+#change timestep to 1
+#plot (1, 5, 10)
+#plot before solver and after solver
